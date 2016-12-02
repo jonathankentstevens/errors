65337f1659bd429d5f81a5bf0f3ebf99dc21aa6c
Jonathan K. Stevens <jonathanstevens89@gmail.com>
2016-12-01 21:13:18 -0700

    updated docs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+[![Go Report Card](https://goreportcard.com/badge/github.com/jonathankentstevens/exception)](https://goreportcard.com/report/github.com/jonathankentstevens/exception)
+[![GoDoc](https://godoc.org/github.com/jonathankentstevens/exception?status.svg)](https://godoc.org/github.com/jonathankentstevens/exception)
+[![Build Status](https://travis-ci.org/jonathankentstevens/exception.svg?branch=master)](https://travis-ci.org/jonathankentstevens/exception)
+
 # exception
 Custom error handler for Golang while keeping the standard for error handling
 
--- exception.go
+++ exception.go
@@ -1,4 +1,4 @@
-//Package Logger is the main package for logging errors for all processes
+//Package exception is a custom error handler to provide better details about an error
 package exception
 
 import (
@@ -10,7 +10,8 @@ import (
 	"time"
 )
 
-type ErrorDetail struct {
+//Exception contains the important information pertaining to an error thrown
+type Exception struct {
 	Msg     string
 	File    string
 	Package string
@@ -22,11 +23,8 @@ type ErrorDetail struct {
 	Stack   string
 }
 
-func (err *ErrorDetail) Error() string {
-	return fmt.Sprintf("%s - %s.%s(%d): %s", err.File, err.Package, err.Method, err.Line, err.Msg)
-}
-
-func New(s string) error {
+//New returns a new instance of Exception
+func New(s string) Exception {
 	hostname, _ := os.Hostname()
 	ipArr, _ := net.LookupHost(hostname)
 	var ip string
@@ -39,5 +37,41 @@ func New(s string) error {
 	pkg := pathArgs[0]
 	method := pathArgs[1]
 
-	return &ErrorDetail{Msg: s, File: file, Package: pkg, Method: method, Line: line, Ip: ip, Host: hostname, Date: time.Now()}
+	return &Exception{Msg: s, File: file, Package: pkg, Method: method, Line: line, Ip: ip, Host: hostname, Date: time.Now()}
+}
+
+
+//Error returns a formatted string displaying the file where the error was thrown, the package it was
+//thrown in, the method, line and, of course, the error message
+func (err Exception) Error() string {
+	return fmt.Sprintf("%s - %s.%s(%d): %s", err.File, err.Package, err.Method, err.Line, err.Msg)
+}
+
+//Stacktrace prints out the stacktrace of the error thrown
+func (err Exception) Stacktrace() string {
+	currentPkg := ""
+	i := 1
+	for {
+		pc, file, line, _ := runtime.Caller(i)
+		path := runtime.FuncForPC(pc).Name()
+		pathArgs := strings.Split(path, ".")
+		pkg := pathArgs[0]
+		method := pathArgs[1]
+
+		if pkg == "runtime" {
+			break
+		}
+
+		if pkg != currentPkg {
+			err.Stack += pkg
+			currentPkg = pkg
+		}
+
+		err.Stack += fmt.Sprintf("\n\t%s - %s(%d)", file, method, line)
+		err.Stack += "\n"
+
+		i++
+	}
+
+	return err.Stack
 }
