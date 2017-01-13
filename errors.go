3e3e2b3e0c8e4c042c574f37b56024a256814aef
Jonathan K Stevens <jstevens@circlepix.com>
2017-01-13 16:25:34 -0700

    added Errorf method

--- errors.go
+++ errors.go
@@ -1,5 +1,5 @@
-//Package exception is a custom error handler to provide better details about an error
-package exception
+//Package errors is a custom error handler to provide better details about an error
+package errors
 
 import (
 	"fmt"
@@ -8,10 +8,12 @@ import (
 	"runtime"
 	"strings"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
-//Exception contains the important information pertaining to an error thrown
-type Exception struct {
+//Error contains the important information pertaining to an error thrown
+type Error struct {
 	Msg     string
 	File    string
 	Package string
@@ -23,8 +25,8 @@ type Exception struct {
 	Stack   string
 }
 
-//New returns a new instance of Exception
-func New(s string) Exception {
+//New returns a new instance of an Error
+func New(s string) error {
 	hostname, _ := os.Hostname()
 	ipArr, _ := net.LookupHost(hostname)
 	var ip string
@@ -37,18 +39,23 @@ func New(s string) Exception {
 	pkg := pathArgs[0]
 	method := pathArgs[1]
 
-	return Exception{Msg: s, File: file, Package: pkg, Method: method, Line: line, Ip: ip, Host: hostname, Date: time.Now()}
+	return Error{Msg: s, File: file, Package: pkg, Method: method, Line: line, Ip: ip, Host: hostname, Date: time.Now()}
+}
+
+//Errorf returns an error based on the formatted string and args given
+func Errorf(format string, args ...interface{}) error {
+	return errors.New(fmt.Sprintf(format, args...))
 }
 
 //Error returns a formatted string displaying the file where the error was thrown, the package it was
 //thrown in, the method, line and, of course, the error message
-func (e Exception) Error() string {
+func (e Error) Error() string {
 	args := strings.Split(e.File, "/")
 	return fmt.Sprintf("%s.%s [%s:%d] - %s", e.Package, e.Method, args[len(args)-1], e.Line, e.Msg)
 }
 
 //Stacktrace prints out the stacktrace of the error thrown
-func (e Exception) Stacktrace() string {
+func (e Error) Stacktrace() string {
 	currentPkg := ""
 	i := 1
 	for {
